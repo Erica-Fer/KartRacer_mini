03ab2886adccc1874eb38b1f442c9ae6e4d726f3
erica-fer <ericafer@uw.edu>
2020-09-11 23:01:52 -0700

    Added functionality for oilslick.

--- Assets/_SANDBOX/KartTest/KartBehavior.cs
+++ Assets/_SANDBOX/KartTest/KartBehavior.cs
@@ -16,8 +16,15 @@ SPACE held - drift
 public class KartBehavior : MonoBehaviour
 {
     Rigidbody rb;
+    float Timer = 0f;
+    
     float TurnSpeed = 180f;
+    float SpinSpeed; // set on collision with obstacles that cause spinning out
+    float SpinLength;
+
     bool IsTurning = false;
+    bool IsSpinning = false;
+    bool WallCollide = false;
 
     float VelMagnitude;
 
@@ -37,7 +44,14 @@ public class KartBehavior : MonoBehaviour
     void FixedUpdate()
     {
         VelMagnitude = rb.velocity.magnitude;
-        Movement();
+        if (IsSpinning)
+        {
+            Spin();
+        }
+        else
+        {
+            Movement();
+        }
     }
 
     #region Physics Movements
@@ -73,8 +87,7 @@ public class KartBehavior : MonoBehaviour
         }
         else
         {
-            rb.constraints = RigidbodyConstraints.None;
-            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            UnlockYRotation();
         }
 
 
@@ -95,9 +108,11 @@ public class KartBehavior : MonoBehaviour
         //SOURCE
         //https://www.youtube.com/watch?v=cqATTzJmFDY
         float ForwardMove = Input.GetAxis("Vertical");
-        float VelThreshold = .5f;
-
-        //? should magnitude be used instead??
+        
+        //? bug where reversing gets a bit fucked with this installed
+        //?so when you turn while going backward, you go opposite direction of what you wanted
+        //float VelThreshold = .5f;
+        ////? should magnitude be used instead??
         //if (ForwardMove < .3 && (rb.velocity.z > VelThreshold || rb.velocity.x > VelThreshold || rb.velocity.y > VelThreshold))
         //{
         //    ForwardMove = .3f;
@@ -142,6 +157,7 @@ public class KartBehavior : MonoBehaviour
     //When button pressed, kart jumps
     //When button pressed while turning, turn is a bit sharper
     //If jump is held, drift initiated
+    // ? TODO
     void Jump()
     {
         //If jump button held for X seconds, call Drift()
@@ -149,18 +165,57 @@ public class KartBehavior : MonoBehaviour
 
     //Initiated by jump, ala mariokart
     //? does this need to be seperate?
+    // ? TODO
     void Drift()
     {
 
     }
+
+    void Spin()
+    {
+        //Quaternion deltaRotate = Quaternion.Euler(0f, 180f, 0f);
+        //rb.MoveRotation(rb.rotation * deltaRotate);
+        //rb.AddTorque(transform.up * SpinSpeed);
+
+        UnlockYRotation();
+
+        //? TODO: Implement spin for when collide w/ tirewall
+        // Current implement makes it all wonky :/
+        float Degrees = 540f;
+        //if(WallCollide)
+        //{
+        //    Degrees *= -1;
+        //}
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles
+                                              + new Vector3(0f, 1 * Degrees * Time.deltaTime * (SpinLength - Timer)/SpinLength, 0f));
+
+        Timer += Time.deltaTime;
+        if(Timer >= SpinLength)
+        {
+            IsSpinning = false;
+            Timer = 0;
+        }
+    }
+
     #endregion
 
     #region Collision Behavior
 
+    //Handles all collision types
+    //Also helps for scenario where kart is going so fast, it goes through colliders
     private void OnCollisionEnter(Collision collision)
     {
         GameObject Col = collision.gameObject;
+        WallCollide = true;
 
+        if (IsSpinning)
+        {
+            return;
+        }
+
+        // If player collides with a tirewall, push player back and spin them some
+        // ? TODO: add spin when player hits wall, and make controlling kart more difficult
         if (Col.tag == "Tirewall")
         {
             Debug.Log("Impact!!!");
@@ -174,7 +229,39 @@ public class KartBehavior : MonoBehaviour
             Debug.Log("Reflected: " + ReflectPosition + "\n");
 
             rb.AddForce(-ReflectPosition*VelMagnitude*.01f, ForceMode.Impulse);
+            
+            //? TODO: finish implementing small spin when collide into wall
+            //IsSpinning = true;          
+            //SpinSpeed = 2;
+            //SpinLength = .5f;
         }
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject Col = collision.gameObject;
+
+        //If collide with oilslick, spin out and lose speed
+        // ? TODO
+        // ? should a kart going a slow speed be able to drive over a slick, or not?
+        if (Col.tag == "Oilslick")
+        {
+            IsSpinning = true;
+            SpinSpeed = 5;
+            // ? TODO add 4 second timer for spinning
+            SpinLength = 2f;
+        }
+    }
+
+    #endregion
+
+    #region Misc
+
+    void UnlockYRotation()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     #endregion
